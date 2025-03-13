079997d57b588f727bbf76c5398208cb3676351b
samuelndjouli997 <samuelndj.997@gmail.com>
2025-03-14 00:02:56 +0100

    tracker

--- src/components/dashboard/step-counter.tsx
+++ src/components/dashboard/step-counter.tsx
@@ -1,6 +1,10 @@
 "use client";
 
 import React, { useState, useEffect, useRef } from "react";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+
+const MAX_STEPS = 5000;
 
 const StepCounter = () => {
   const [steps, setSteps] = useState(0);
@@ -9,7 +13,6 @@ const StepCounter = () => {
 
   const handleMotion = (event: DeviceMotionEvent) => {
     const acceleration = event.accelerationIncludingGravity;
-
     if (!acceleration) return;
 
     const x = acceleration.x ?? 0;
@@ -21,8 +24,11 @@ const StepCounter = () => {
     const deltaZ = Math.abs(z - lastAcceleration.current.z);
 
     if (deltaX + deltaY + deltaZ > 5) {
-      setSteps((prevSteps) => prevSteps + 1);
-      localStorage.setItem("steps", (steps + 1).toString());
+      setSteps((prevSteps) => {
+        const newSteps = prevSteps + 1;
+        localStorage.setItem("steps", newSteps.toString());
+        return newSteps;
+      });
     }
 
     lastAcceleration.current = { x, y, z };
@@ -45,7 +51,6 @@ const StepCounter = () => {
         console.error("Erreur lors de la demande de permission :", error);
       }
     } else {
-      // Android ou navigateurs qui ne nécessitent pas de permission explicite
       setHasPermission(true);
       window.addEventListener("devicemotion", handleMotion);
     }
@@ -60,10 +65,32 @@ const StepCounter = () => {
     };
   }, [hasPermission]);
 
+  const progressPercentage = Math.min((steps / MAX_STEPS) * 100, 100);
+
   return (
-    <div className="p-6 bg-white shadow-lg rounded-xl flex flex-col items-center">
-      <h3 className="text-xl font-semibold">Compteur de pas</h3>
-      <p className="text-4xl font-bold text-blue-500">{steps}</p>
+    <div className="p-6 bg-[#0F1334] shadow-lg rounded-xl flex flex-col items-center">
+
+      {/* Container pour la jauge semi-circulaire */}
+      <div className="relative w-40 h-20 overflow-hidden">
+        <CircularProgressbar
+          value={progressPercentage}
+          maxValue={100}
+          strokeWidth={10}
+          styles={buildStyles({
+            rotation: 1 / 2 + 1 / 4, // Rotation pour n'afficher que le haut
+            strokeLinecap: "round",
+            pathColor: "#4CAF50",
+            trailColor: "#374151",
+            backgroundColor: "#0F1334",
+          })}
+        />
+      </div>
+
+      {/* Texte centré sous la jauge */}
+      <div className="mt-4 flex gap-20 justify-between text-white">
+        <p className="text-sm">{steps} pas</p>
+        <p className="text-sm ml-5">{MAX_STEPS} pas</p>
+      </div>
 
       {!hasPermission && (
         <button
